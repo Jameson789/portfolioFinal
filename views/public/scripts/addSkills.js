d902d55291a0bdf6fcc43daa4a86d5179ac09ed0
Jameson Farmer <jamesonfarmer@mac.lan>
2024-12-09 23:42:36 -0800

    apapting stylesheet to everyhting and other changes

--- views/public/scripts/addSkills.js
+++ views/public/scripts/addSkills.js
@@ -1,13 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const addSkillButton = document.getElementById('add-skill-button');
-    const skillContainer = document.getElementById('skill-container');
+const addSkillButton = document.getElementById('add-skill-button');
+const skillContainer = document.getElementById('skill-container');
 
-    addSkillButton.addEventListener('click', () => {
-        const newSkillInput = document.createElement('input');
-        newSkillInput.type = 'text';
-        newSkillInput.name = 'skills';
-        newSkillInput.className = 'input-field';
-        newSkillInput.placeholder = 'Enter another skill';
-        skillContainer.appendChild(newSkillInput);
-    });
+addSkillButton.addEventListener('click', () => {
+    const newSkillInput = document.createElement('input');
+    newSkillInput.type = 'text';
+    newSkillInput.name = 'skills';
+    newSkillInput.className = 'input-field';
+    newSkillInput.placeholder = 'Enter another skill';
+    skillContainer.appendChild(newSkillInput);
 });
+
